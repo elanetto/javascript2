1310e0036c262c3f2082c95dac3b788bb98a28c3
elanetto <101054274+elanetto@users.noreply.github.com>
2024-09-04 11:17:07 +0200

    Class with Dean about webpack

---
+++ module-3/node/joke-game-cdn/app.js
@@ -0,0 +1,8 @@
+console.log("Hello 🌟");
+
+const jokes = ["joke 1", "joke 2", "joke 3", "joke 4", "joke 5"];
+
+// want to do something, but I don't like it
+jokes.sort();
+
+// I need better options
